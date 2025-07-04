840d25209039bd26c1b0280b17d3db93dabaf1e9
Stephen Pleasant <stephenpleasant1@gmail.com>
2025-07-04 23:33:57 +0100

    new

--- Backend/index.js
+++ Backend/index.js
@@ -13,14 +13,14 @@ import movieRoutes from './routes/movieRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+ 
 // ✅ Middleware
 app.use(cors());
 app.use(express.json());
 
 // ✅ Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/dashboard', movieRoutes);
+app.use('/api/movie', movieRoutes);
 
 // ✅ Health check route
 app.get('/', (req, res) => {
