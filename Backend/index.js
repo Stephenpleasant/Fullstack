eb279a9acb163f668289610052c0452e2ee85fc3
Stephen Pleasant <stephenpleasant1@gmail.com>
2025-07-08 13:50:10 +0100

    configuring the port

--- Backend/index.js
+++ Backend/index.js
@@ -29,5 +29,5 @@ app.get('/', (req, res) => {
 
 // ✅ Connect to MongoDB and start server
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 });
